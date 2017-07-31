30c043262645eb8fc94ee67d54bcfb7c1f47f8e6
raphael <raphael_smadja@hotmail.com>
2017-07-31 08:41:33 +0200

    Installation des packages nodejs et 1ere fenetre accueil java

--- SkullOfMusic/src/sample/Main.java
+++ SkullOfMusic/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Skull Of Music");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("file:/../ressources/logo.png"));
         primaryStage.show();
     }
 
